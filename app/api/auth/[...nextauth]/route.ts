ec8596764087c14c22171b4e715dbadadcc2aebb
Timea Fodor <zsigovszkit@gmail.com>
2025-05-07 18:16:38 +0300

    added auth config

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,24 +1,5 @@
+import { authOptions } from "@/app/lib/auth.config";
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import type { NextAuthOptions } from "next-auth";
-
-export const authOptions: NextAuthOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-  ],
-  secret: process.env.NEXTAUTH_SECRET,
-  pages: {
-    signIn: "/signin",
-  },
-  callbacks: {
-    async signIn({ user }) {
-      return user.email === process.env.GOOGLE_EMAIL;
-    },
-  },
-};
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
---
+++ app/lib/auth.config.ts
@@ -0,0 +1,20 @@
+import GoogleProvider from "next-auth/providers/google";
+import type { NextAuthOptions } from "next-auth";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  pages: {
+    signIn: "/signin",
+  },
+  callbacks: {
+    async signIn({ user }) {
+      return user.email === process.env.GOOGLE_EMAIL;
+    },
+  },
+};
